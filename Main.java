9a738c7277e49028d86cc9fa585902aed58ec536
oleksiy131 <60301863+oleksiy131@users.noreply.github.com>
2020-10-22 08:33:57 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945L));
+        System.out.println(getDurationString(-1));
+    }
+
+    public static String getDurationString(long minutes, long seconds) {
+        if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
+            return INVALID_VALUE_MESSAGE;
+        }
+
+        long hours = (int) minutes / 60;
+        long remainderMins = minutes % 60;
+
+        return hours + "h " + remainderMins + "m " + seconds + "s";
+    }
+
+    public static String getDurationString(long seconds) {
+        if (seconds < 0) {
+            return INVALID_VALUE_MESSAGE;
+        }
+
+        long mins = seconds / 60;
+        long remainderSeconds = seconds % 60;
+
+        return getDurationString(mins, remainderSeconds);
+    }
+}
